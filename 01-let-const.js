c8b0f8e8525834bcdba2a9e19936abdbf30dd442
hientran-sudo <hien.tran@student.fairfield.edu>
2024-01-04 16:23:25 -0500

    update

--- 01-let-const.js
+++ 01-let-const.js
@@ -1,4 +1,7 @@
 var i = 1;
-for (var i = 1; i <= 5; i++) {
+for (var i_1 = 1; i_1 <= 5; i_1++) {
 }
+var i = 2;
 console.log(i);
+var start = 10;
+console.log(start);
--- 01-let-const.ts
+++ 01-let-const.ts
@@ -1,5 +1,9 @@
-var i = 1;
-for (var i = 1; i <= 5 ; i++){
+let i = 1;
+for (let i = 1; i <= 5 ; i++){
 
 }
+let i = 2;
 console.log(i);
+
+const start = 10;
+console.log(start);
